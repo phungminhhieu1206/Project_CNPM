634b03a41d0f271cc317a61a9963eb8206ce23aa
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2020-12-15 02:14:28 +0700

    create Person class

--- ProjectCNPM_Covid19_Management/src/com/nhom12/covid19/entity/Person.java
+++ ProjectCNPM_Covid19_Management/src/com/nhom12/covid19/entity/Person.java
@@ -5,10 +5,41 @@
  */
 package com.nhom12.covid19.entity;
 
+import com.nhom12.covid19.MY_CONNECTION;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JComboBox;
+
 /**
- *
- * @author HieuPhung
+ * 
+ * @author PhamVanHanh
  */
 public class Person {
+    MY_CONNECTION my_connection = new MY_CONNECTION();
+    
+    // method to search person by cmt
+    public boolean searchPerson(String cmt){
+        return true;
+    }
+    // method to add new person
+    public boolean addPerson(String name, String birthday, String gender, String bhyt, String bhyt_num, String cmt, String ho_khau, String phone, String email, String address){
+        
+        
+        return true;
+        
+    }
+    
+    public boolean editPerson(int idPerson, String name, String birthday, String gender, String bhyt, String bhyt_num, String cmt, String ho_khau, String phone, String email, String address) {
+          
+        return true;
+           
+    }
+    public boolean removePerson(int idPerson){
+        
+        return true;
+    }
     
 }
