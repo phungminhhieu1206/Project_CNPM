22e93fc8bf9fc8e959ee28220904eab24fbd94f6
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2020-12-15 22:39:10 +0700

    done editPerson method of Person class

--- ProjectCNPM_Covid19_Management/src/com/nhom12/covid19/entity/Person.java
+++ ProjectCNPM_Covid19_Management/src/com/nhom12/covid19/entity/Person.java
@@ -34,7 +34,35 @@ public class Person {
     
     public boolean editPerson(int idPerson, String name, String birthday, String gender, String bhyt, String bhyt_num, String cmt, String ho_khau, String phone, String email, String address) {
           
-        return true;
+
+        PreparedStatement ps;
+        String updateQuery = "UPDATE `people` SET `name`=?,`birthday`=?,`gender`=?,`bhyt`=?,`bhyt_num`=?,`cmt`=?,`ho_khau`=?,`phone`=?,`email`=?,`address`=? WHERE `id`=?";
+        
+        try {
+            // add client on mysql database
+            ps = my_connection.createConnection().prepareStatement(updateQuery);
+            
+            // theo thu tu dau ?
+            ps.setString(1, name);
+            ps.setString(2, birthday);
+            ps.setString(3, gender);
+            ps.setString(4, bhyt);
+            ps.setString(5, bhyt_num);
+            ps.setString(6, cmt);
+            ps.setString(7, ho_khau);
+            ps.setString(8, phone);
+            ps.setString(9, email);
+            ps.setString(10, address);
+            ps.setInt(11, idPerson);
+            
+            return (ps.executeUpdate() > 0);
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Person.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
            
     }
     public boolean removePerson(int idPerson){
