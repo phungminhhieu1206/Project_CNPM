925f7f08996c175ffe226ac3a2d652fe536bb6d9
Nam Hoang <55123518+namnh194@users.noreply.github.com>
2020-12-17 01:42:43 +0700

    add DichTeForm.java

---
+++ ProjectCNPM_Covid19_Management/src/com/nhom12/covid19/forms/input/Person.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nhom12.covid19.forms.input;
+
+/**
+ *
+ * @author Dell
+ */
+class Person {
+
+    boolean searchPerson(String cmt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
